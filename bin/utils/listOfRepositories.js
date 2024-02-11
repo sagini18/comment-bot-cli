bef12caa50d46fc17f7aafead6a39aab6f5be213
sagini18 <saginisaju@gmail.com>
2024-02-11 15:49:14 +0530

    listofrepo refactored

--- bin/utils/listOfRepositories.js
+++ bin/utils/listOfRepositories.js
@@ -1,31 +1,27 @@
 import { readTokenFromFile } from "./install.js";
 import axios from "axios";
 
-const listOfRepositories = () => {
+async function listOfRepositories() {
   const token = readTokenFromFile();
   if (!token) {
     console.log("Token not found. Please install the bot first.");
     return;
   }
-  
-  axios
-    .get("https://api.github.com/user/repos", {
-      headers: {
-        Authorization: `token ${token}`,
-      },
-    })
-    .then((response) => {
-      response.data.forEach((element) => {
-        console.log(element.name);
-      });
-    })
-    .catch((error) => {
-      if (error.response?.status === 401) {
-        console.log("Invalid token. Please install the bot again.");
-      } else {
-        console.log(error.response?.data?.message);
-      }
-    });
-};
+
+  const response = await axios.get("https://api.github.com/user/repos", {
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  });
+
+  response.data.forEach((element) => {
+    console.log(element.name);
+  });
+
+  if (response.status === 401) {
+    console.log("Invalid token. Please install the bot again.");
+    return;
+  }
+}
 
 export { listOfRepositories };
